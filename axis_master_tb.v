2d9f26bb88e26e71397e46c1d8e5480244cf79b2
Sonu Tripathi <110840084+8307820421@users.noreply.github.com>
2024-08-26 15:51:47 +0530

    Update axis_master_tb.v

--- axis_master_tb.v
+++ axis_master_tb.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-// logic developed by me:
+// logic developed by me: This is AXIS based master not realted to BRAM and other things.
 
 module axis_master_tb();
 // input signal as per problem statement
@@ -65,8 +65,8 @@ initial begin
 m_axis_rstn = 0;
 repeat(10)@(posedge m_axis_clk)
 
-for (i= 0 ; i<5 ; i= i+1) // counter logic // for each trransfer having 4 byte of data
-begin
+ for (i= 0 ; i<5 ; i= i+1) // counter logic // for each trransfer having 4 byte of data// with respect to i with in five clock pulse the tdata will be updated based upon counter
+begin                      // as counter is counting up to 3 byte hence when it will last then t_last asserted or active and t_valid after this deasserted as well as tready.
 
 @(posedge m_axis_clk)
 
