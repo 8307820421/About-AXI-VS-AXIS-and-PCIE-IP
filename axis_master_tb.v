f732ec1913590a9a12b0c0b4fdb11b02ad8d1fb9
Sonu Tripathi <110840084+8307820421@users.noreply.github.com>
2024-08-28 13:10:59 +0530

    Update axis_master_tb.v

--- axis_master_tb.v
+++ axis_master_tb.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-// logic developed by me: This is AXIS based master not realted to BRAM and other things.
+// logic developed by me: This is AXIS based master not realted to BRAM and other things // correct test bench.
 
 module axis_master_tb();
 // input signal as per problem statement
@@ -55,32 +55,88 @@ reg m_axis_tready;
 // adding stimulus for clock
 
 
-always #10 m_axis_clk = ~m_axis_clk;
-
-integer i;
-
-// stimuli for data transfer logic
-initial begin
-
-m_axis_rstn = 0;
-repeat(10)@(posedge m_axis_clk)
-
- for (i= 0 ; i<5 ; i= i+1) // counter logic // for each trransfer having 4 byte of data// with respect to i with in five clock pulse the tdata will be updated based upon counter
-begin                      // as counter is counting up to 3 byte hence when it will last then t_last asserted or active and t_valid after this deasserted as well as tready.
-
-@(posedge m_axis_clk)
-
- m_axis_rstn= 1;
- m_axis_tready = 1'b1;
- new_data = 1; // oone bit for enabling the new data 
- din = $random;
- 
- @(negedge m_axis_tlast) 
- m_axis_tready = 1'b0;
+   integer i;
+   always #10 axis_clk = ~axis_clk;
    
-end
-
-end
+   // stimuli for input reg
+   initial begin
+ 
+   t_valid = 1'b0;
+   t_data =  0;
+   t_last = 1'b0;
+   reset = 1'b1;
+   t_keep = {keep_width{1'b0}};
+   repeat(5) @(posedge axis_clk) ;
+    
+    // stimuli for high reset and t_valid
+    reset = 1'b0; 
+    for (i = 0; i<mem_size_depth ; i= i+1) // for indexing based on that data will go
+    begin
+    @(posedge axis_clk); // clk for loop
+    
+    // tvalid
+    t_valid = 1'b1;
+    t_keep[i] = {keep_width{1'b1}};
+    t_data = $urandom;
+    bram_dout = bram_data;
+     
+     //@(posedge axis_clk);  // clk for tvalid//tdta // this clock will lead to t_ready high for short time hence adjust the logic if required//
+    
+    end 
+   @(posedge axis_clk); // clk for entire loop
+    
+    t_last = 1'b1;
+    @(posedge axis_clk); // clk for t_last
+    
+    t_last = 0;
+    t_valid = 0;
+    @(posedge axis_clk); // clk for deasserted the tlast and tvalid
+      $finish;
+  
+   end
 endmodule
 
+
+
+// here there is another approach where address counter is incremented up to mem_size but at that time with one posedge only 512 bit data transmitted w.r.t i (index).
+// put this inside the initial block.
+
+//// Clock generation
+////initial axis_clk = 1'b1;
+//always #10 axis_clk = ~axis_clk;
+
+//integer i;
+// // Stimulus generation
+//initial begin
+//// Initialize inputs
+//reset = 1;
+//t_valid = 1'b0;
+//t_last = 1'b0;
+//t_keep = {keep_width{1'b0}};
+//repeat(10) @(posedge axis_clk);
+
+ 
+// for (i = 0; i<= (mem_size_depth-1) ; i= i+1) begin
+
+//@(posedge axis_clk);
+//reset = 0;
+//t_valid = 1'b1;
+//t_keep[i] = {keep_width{1'b1}} ; // based upon t_keep validity
+//t_data = $random;
+//t_last = 1'b0;
+
+
+//@(posedge t_ready);
+//t_last = 1'b0;
+
+//@(negedge t_ready);
+//t_valid= 1'b0;
+//t_last = 1'b1;
+
+
+
+//end
+ 
+ 
+//end 
 
